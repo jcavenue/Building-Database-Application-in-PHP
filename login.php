966d10e4e5f2319834a3ad7bccdd965eb1e24f51
Suzaku <jcfababeir09@gmail.com>
2021-06-17 21:54:08 +0800

    Login Security Screen

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
-	require_once "connect/pdo.php";
+	require_once 'validate.php';
 
-	if(isset($_POST['Login']) && isset($_POST['email']) && isset($_POST['pass'])){
-	
-
+	if(isset($_POST['cancel'])){
+		header('Location: index.php');
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -14,25 +12,39 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
+		<title>f3a47880</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	<body>
-		<div class="container m-3 p-3">
+		<div class="container-fluid mx-5 mt-4 px-5">
 			<h2>Please Log in</h2>
-			<form method="POST" class="form">
+			<?php
+				if(isset($_POST['Login'])){
+					if(isset($_POST['who']) && empty($_POST['pass'])){
+						echo '<p class="text-danger small">Email and password are required</p>';
+					} else if(validate($_POST['pass']) == false){
+						echo '<p class="text-danger small">Incorrect password</p>';
+					} else if (filter_var($_POST['who'], FILTER_VALIDATE_EMAIL) == false){
+						echo '<p class="text-danger small">Email must have an at-sign (@)</p>';
+					} else {
+						header("Location: autos.php?name=".urlencode($_POST['who']));
+					}
+				}
+
+			?>
+			<form method="post" class="form">
 				<table>
 					<tr>
-						<td><label class="form-label small" for="email">Email</label></td>
-						<td><input type="text" name="email" id="email"></td>
+						<td><label class="form-label small" for="email">User Name</label></td>
+						<td><input type="text" name="who" id="email"></td>
 					</tr>
 					<tr>
 						<td><label class="form-label small"for="pass">Password</label></td>
 						<td><input type="pass" name="pass" id="pass"></td>
 					</tr>
 				</table><br>
-				<input type="submit" name="Login= "value="Log In" class="btn btn-primary btn-sm">
-				<input type="submit" name="cancel" value="cancel" class="btn btn-secondary btn-sm">
+				<input type="submit" name="Login" value="Log In" class="btn btn-primary btn-sm">
+				<input type="submit" name="cancel" value="Cancel" class="btn btn-secondary btn-sm">
 			</form>
 		</div>
 		<script src="js/bootstrap.min.js"></script>
---
+++ validate.php
@@ -0,0 +1,11 @@
+<?php
+	function validate($pass){
+		$salt = 'XyZzy12*_';
+		$stored_hash = "1a52e17fa899cf40fb04cfc42e6352f1";
+		$md5 = hash('md5', $salt . $pass);
+		if($md5 !== $stored_hash){
+			return false;
+		}
+		return true;
+	}
+?>
